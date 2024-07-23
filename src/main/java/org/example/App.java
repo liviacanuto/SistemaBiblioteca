3b6f57568fe34a4ec4922d6d8f71310c0f0530f6
liviacanuto <canutto.livia@gmail.com>
2024-07-23 15:46:51 -0300

    mais um teste

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import org.example.ExternalBook.BookAdapter;
+import org.example.ExternalBook.ExternalBookAdapter;
+
 public class App {
     public static void main(String[] args) {
         CenarioManager.popularBanco();
         LibraryFacade mediator = new LibraryMediator();
+        BookAdapter ex1 = new ExternalBookAdapter(3, "Livro externo", true,"Autor", new Category("Externo"));
 
         System.out.println("******INICIO DOS TESTES*******");
         System.out.println("\n****** EMPRÉSTIMO DE LIVRO *******");
@@ -17,6 +21,9 @@ public class App {
 
         System.out.println("\n******ENCONTRAR LIVROS + DETALHES*******");
         System.out.println(mediator.searchBook(1001));
+
+        System.out.println("\n******VERIFICAR SE LIVRO EXTERNO POSSUI UMA CATEGORIA ESPECIFICA*******");
+        System.out.println(ex1.temCategoria("Externo"));
 
         System.out.println("\n******ENCONTRAR USUARIOS + HISTÓRICO DE EMPRÉSTIMO*******");
         System.out.println(mediator.userLoanHistory(1));
